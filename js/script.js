dbbb730f2db3aca2725c9372a26fc04aa71d8ff3
Darth Vader <danieltarancon@gmail.com>
2023-05-21 19:53:55 +0200

    Updated

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ project 1 - A Random Quote Generator
 
 quotes = [{quote:'I have no special talent. I am only passionately curious', source:'Albert Einstein', citation:'Newspaper interview', year:1930}, {quote:'love all, trust a few, do wrong to none.', source:'William Shakespeare', citation:'All is Well That Ends Well', year:1605},
     {quote:'Life is like riding a bicycle. To keep your balance, you must keep moving.', source:'Albert Einstein', citation:'Letter to his son', year:1930},
-    {quote:'Do not live the same year 75 times and call it a life.', source:'Robin Sharma', citation:'Public Speech in Toronto', year:2010}, {quote:'That which does not kill us makes us stronger.', source:'Friedrich Nietzsche', citation:'Twilight of the Idols', year:1889}]
+    {quote:'Do not live the same year 75 times and call it a life.', source:'Robin Sharma', citation:'Public Speech in Toronto', year:2010, advice:'Life Advice'}, {quote:'That which does not kill us makes us stronger.', source:'Friedrich Nietzsche', citation:'Twilight of the Idols', year:1889}]
 
 
 
@@ -41,6 +41,12 @@ function printQuote(){
 
     display_quote += `<span class="year">${random_quote.year}</span>`
     }
+
+    if (random_quote.advice){
+
+    display_quote += `<em class="year">${random_quote.advice}</em>`
+    }
+
     display_quote + `</p>`
 
     document.getElementById('quote-box').innerHTML = display_quote;
