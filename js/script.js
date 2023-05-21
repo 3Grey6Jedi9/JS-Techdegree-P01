07d0f10b49fbbe873bc2739807c9a5a820cc5961
Darth Vader <danieltarancon@gmail.com>
2023-05-21 17:30:36 +0200

    Updated

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 
-quotes = [{quote:'I have no special talent. I am only passionately curious', author:'Albert Einstein'}, {quote:'love all, trust a few, do wrong to none.', author:'William Shakespeare'},
-    {quote:'Life is like riding a bicycle. To keep your balance, you must keep moving.', author:'Albert Einstein'},
-    {quote:'Do not live the same year 75 times and call it a life.', author:'Robin Sharma'}, {quote:'That which does not kill us makes us stronger.', author:'Friedrich Nietzsche'}]
+quotes = [{quote:'I have no special talent. I am only passionately curious', source:'Albert Einstein', citation:'Newspaper interview', year:1930}, {quote:'love all, trust a few, do wrong to none.', source:'William Shakespeare', citation:'All is Well That Ends Well', year:1605},
+    {quote:'Life is like riding a bicycle. To keep your balance, you must keep moving.', source:'Albert Einstein', citation:'Letter to his son', year:1930},
+    {quote:'Do not live the same year 75 times and call it a life.', source:'Robin Sharma', citation:'Public Speech in Toronto', year:2010}, {quote:'That which does not kill us makes us stronger.', source:'Friedrich Nietzsche', citation:'Twilight of the Idols', year:1889}]
 
 
 
