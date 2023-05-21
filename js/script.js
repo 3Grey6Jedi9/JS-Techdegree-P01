7a2f027ebd64c5a8bb3b40fc0ac3db6ffcb0c484
Darth Vader <danieltarancon@gmail.com>
2023-05-21 17:17:30 +0200

    Updated

--- js/script.js
+++ js/script.js
@@ -7,9 +7,10 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+
+quotes = [{quote:'I have no special talent. I am only passionately curious', author:'Albert Einstein'}, {quote:'love all, trust a few, do wrong to none.', author:'William Shakespeare'},
+    {quote:'Life is like riding a bicycle. To keep your balance, you must keep moving.', author:'Albert Einstein'},
+    {quote:'Do not live the same year 75 times and call it a life.', author:'Robin Sharma'}, {quote:'That which does not kill us makes us stronger.', author:'Friedrich Nietzsche'}]
 
 
 
